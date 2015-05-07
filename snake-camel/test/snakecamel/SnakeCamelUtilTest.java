5b00d03a01e9261a2e53cecced70629b733729c2
sakikoseida <g12948ss+git@tsuda.ac.jp>
2015-05-07 16:14:15 +0900

    修正した。

--- snake-camel/test/snakecamel/SnakeCamelUtilTest.java
+++ snake-camel/test/snakecamel/SnakeCamelUtilTest.java
@@ -1,14 +1,40 @@
 package snakecamel;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class SnakeCamelUtilTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void 先頭小文字を大文字へ変換() {
+		SnakeCamelUtil snacame =new SnakeCamelUtil();
+		String expected = "Abc_def";
+		String actual = snacame.capitalize("abc_def");
+		assertThat(actual, is(expected));
 	}
-
-}
+	
+	@Test
+	public void 先頭大文字を小文字へ変換() {
+		SnakeCamelUtil snacame =new SnakeCamelUtil();
+		String expected = "abcDef";
+		String actual = snacame.uncapitalize("AbcDef");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void スネークをキャメルへ変換() {
+		SnakeCamelUtil snacame =new SnakeCamelUtil();
+		String expected = "AbcDef";
+		String actual = snacame.snakeToCamelcase("abc_def");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void キャメルをスネークへ変換(){
+		SnakeCamelUtil snacame =new SnakeCamelUtil();
+		String expected = "abc_defg_hi";
+		String actual = snacame.camelToSnakecase("AbcDefgHi");
+		assertThat(actual, is(expected));
+	}		
+}	
